a2db49dfb78976d7cf6fe23bad50d453851650ff
asexto <a.sexto@gmail.com>
2025-05-14 19:39:43 +0200

    Añadido movimiento básico del héroe

--- src/bloqr.py
+++ src/bloqr.py
@@ -3,8 +3,12 @@ from pygame.locals import *
 
 WORLD_WIDTH = 256
 WORLD_HEIGHT = 256
+BLOCK_WIDTH = 32
+BLOCK_HEIGHT = 24
 
-pyramid = []    # matriz que contiene información sobre todos los bloques del juego
+pyramid = [] # matriz que contiene información sobre todos los bloques del juego
+hero_row = 0
+hero_col = 0
 
 def mi_error(mensaje):
     print(f"ERROR: {mensaje}")
@@ -19,7 +23,7 @@ def get_new_scale_factor(display_x_resolution, display_y_resolution):
     return new_scale_factor
 
 def init_game():
-    global pyramid
+    global pyramid, hero_row, hero_col
     pyramid = []
     floor_columns = 7
     for floor_number in range(7):
@@ -28,10 +32,12 @@ def init_game():
             floor.append(0)
         pyramid.append(floor)
         floor_columns -= 1
-    print(pyramid)
+    #print(pyramid)
+    hero_row = 6
+    hero_col = 0
 
-def pantalla_menu():
-    global pantalla, scale_factor
+def screen_menu():
+    global screen, scale_factor
     # lectura cola de eventos
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -43,10 +49,11 @@ def pantalla_menu():
         elif event.type == pygame.KEYDOWN:
             if event.key == K_SPACE:
                 init_game()
-                pantalla = "partida"
+                screen = "match"
 
     draw_surface.fill((0, 0, 0))
-    draw_surface.blit(title_img, (100, 100))
+    draw_surface.blit(menu_title_img, ((WORLD_WIDTH - menu_title_img.get_width()) / 2, 100))
+    draw_surface.blit(menu_subtitle_img, ((WORLD_WIDTH - menu_subtitle_img.get_width()) / 2, 150))
 
 
 def draw_block(x, y):
@@ -56,11 +63,19 @@ def draw_block(x, y):
 def draw_pyramid(x, y):
     for floor_number, blocks in enumerate(pyramid):
         for col_number in range(len(blocks)):
-            draw_block(x + 32 * col_number + 16 * floor_number, y + (6 - floor_number)  * 24)
+            draw_block(x + BLOCK_WIDTH * col_number + floor_number * BLOCK_WIDTH / 2, y + (6 - floor_number)  * BLOCK_HEIGHT)
 
 
-def pantalla_partida():
-    global scale_factor
+def cell_exists(row, col):
+    return 0 <= row <= 6 and 0 <= col <= 6 - row
+
+
+def draw_hero(hero_row, hero_col, x_pyramid, y_pyramid):
+    draw_surface.blit(hero_img, (x_pyramid + block_img_shift_x - 1 + hero_col * BLOCK_WIDTH + hero_row * BLOCK_WIDTH / 2, y_pyramid + block_img_shift_y + 142 - hero_row * BLOCK_HEIGHT))
+
+
+def screen_match():
+    global scale_factor, hero_row, hero_col
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -69,9 +84,28 @@ def pantalla_partida():
         elif event.type == VIDEORESIZE:
             new_x_resolution, new_y_resolution = event.dict['size']
             scale_factor = get_new_scale_factor(new_x_resolution, new_y_resolution)
+        elif event.type == pygame.KEYDOWN:
+            dest_hero_row = hero_row
+            dest_hero_col = hero_col
+            if event.key == K_q:  # Diagonal arriba-izquierda
+                dest_hero_row = hero_row + 1
+                dest_hero_col = hero_col - 1
+            elif event.key == K_w:  # Diagonal arriba-derecha
+                dest_hero_row = hero_row + 1
+                dest_hero_col = hero_col
+            elif event.key == K_a:  # Diagonal abajo-izquierda
+                dest_hero_row = hero_row - 1
+                dest_hero_col = hero_col
+            elif event.key == K_s:    # Diagonal abajo-derecha
+                dest_hero_row = hero_row - 1
+                dest_hero_col = hero_col + 1
+            if cell_exists(dest_hero_row, dest_hero_col):
+                hero_row = dest_hero_row
+                hero_col = dest_hero_col
 
     draw_surface.fill((0, 0, 0))
     draw_pyramid(16, 32)
+    draw_hero(hero_row, hero_col, 16, 32)
 
 
 pygame.init()
@@ -84,18 +118,22 @@ display_surface = pygame.display.set_mode((WORLD_WIDTH * scale_factor, WORLD_HEI
 draw_surface = pygame.Surface((WORLD_WIDTH, WORLD_HEIGHT),  HWSURFACE | DOUBLEBUF)
 
 pygame.display.set_caption('bloqr')
-font_title = pygame.font.Font("assets/arcade_i.ttf", 18)
-title_img = font_title.render("bloqr", False, (0, 255, 0))
+font_title = pygame.font.Font("assets/arcade_i.ttf", 28)
+font_paragraph = pygame.font.Font("assets/arcade_i.ttf", 11)
+menu_title_img = font_title.render("bloqr", False, (0, 255, 0))
+menu_subtitle_img = font_paragraph.render("press space to start", False, (0, 255, 0))
 block_img = pygame.image.load('assets/block.png')
 block_img_shift_x = -96
 block_img_shift_y = -160
-
-pantalla = "menu"
+hero_img = pygame.image.load('assets/hero.png')
+hero_img_shift_x = -98
+hero_img_shift_y = -144
+screen = "menu"
 while True:  # main game loop
-    if pantalla == "menu":
-        pantalla_menu()
-    elif pantalla == "partida":
-        pantalla_partida()
+    if screen == "menu":
+        screen_menu()
+    elif screen == "match":
+        screen_match()
     else:
         mi_error("Pantalla no válida")
 
